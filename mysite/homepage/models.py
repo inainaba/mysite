03d154bfd7f9b07963c8a4945222660ec5c1a3d4
inaba1203mpka <koumpka@gmail.com>
2019-03-03 02:25:01 +0900

    Blogモデル追加

--- mysite/homepage/models.py
+++ mysite/homepage/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-# Create your models here.
+""" 自分の情報 """
 class Myinfo(models.Model):
     name = models.CharField("名前",max_length=30)
     twitter = models.URLField("Twitter")
@@ -11,6 +11,29 @@ class Myinfo(models.Model):
     def __str__(self):
         return self.name
 
+
+""" ミニブログ用 """
 class Blog(models.Model):
     title = models.CharField("タイトル",max_length=100)
-    
+    date_birth = models.DateField(auto_now_add=True)
+    date_update = models.DateField(auto_now=True)
+    picture = models.ImageField(upload_to='images/', null=True, blank=True)
+    text = models.TextField("本文")
+    is_public = models.BooleanField(default=True)
+    #tag
+
+
+""" タグ """
+class Tag(models.Model):
+    pass
+
+
+
+""" ポートフォリオページ用 """
+class Work(models.Model):
+    pass
+
+
+""" ジャンル """
+class Subject(models.Model):
+    pass
