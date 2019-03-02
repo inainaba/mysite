ee8e568ce15d82932237c7145eb8761c495a84a7
inaba1203mpka <koumpka@gmail.com>
2019-03-02 23:47:05 +0900

    myinfoモデル追加

--- mysite/homepage/models.py
+++ mysite/homepage/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Myinfo(models.Model):
+    name = models.CharField("名前",max_length=30)
+    twitter = models.URLField("Twitter")
+    github = models.URLField("GitHub")
+    email = models.EmailField("E-mail")
+    location = models.CharField("所在地"max_length=50)
+
+    def __str__(self):
+        return self.name
+
+class Blog(models.Model):
+    title = models.CharField("タイトル",max_length=100)
+    
