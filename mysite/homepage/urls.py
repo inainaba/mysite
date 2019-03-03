0fcc5146134a0c729ebbe077b095162686f069c5
inaba1203mpka <koumpka@gmail.com>
2019-03-03 10:29:29 +0900

    template追加

--- mysite/homepage/urls.py
+++ mysite/homepage/urls.py
@@ -2,5 +2,8 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    #path('',views. ),
+    path('', views.Top.as_view(), name='top'),
+    path('info/', views.Myinfo.as_view(), name='myinfo')
+    path('myblog/', views.Myblog.as_view(), name='myblog'),
+    path('works/', views.Works.as_view(), name='works'),
 ]
--- mysite/homepage/views.py
+++ mysite/homepage/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import *
 
-# Create your views here.
+
+class Top(generic.TemplateView):
+    template_name = "homepage/top"
+
+
+class Myinfo(generic.TemplateView):
+    template_name = "homepage/myinfo"
+
+
+class Myblog(generic.ListView):
+    template_name = "homepage/myblog"
+
+
+class Works(generic.ListView):
+    template_name = "homepage/works"
