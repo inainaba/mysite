ace52acd0a9864aecd55df1eb3d3cc2c2ae45856
inaba1203mpka <koumpka@gmail.com>
2019-03-03 09:18:36 +0900

    models.py追加

--- mysite/homepage/models.py
+++ mysite/homepage/models.py
@@ -3,9 +3,10 @@ from django.db import models
 """ 自分の情報 """
 class Myinfo(models.Model):
     name = models.CharField("名前",max_length=30)
-    twitter = models.URLField("Twitter")
-    github = models.URLField("GitHub")
-    email = models.EmailField("E-mail")
+    twitter = models.URLField("Twitter",null=True, blank=True)
+    github = models.URLField("GitHub",null=True, blank=True)
+    facebook = models.URLField("Facebook",null=True, blank=True)
+    email = models.EmailField("E-mail",null=True, blank=True)
     location = models.CharField("所在地"max_length=50)
 
     def __str__(self):
@@ -15,25 +16,31 @@ class Myinfo(models.Model):
 """ ミニブログ用 """
 class Blog(models.Model):
     title = models.CharField("タイトル",max_length=100)
-    date_birth = models.DateField(auto_now_add=True)
-    date_update = models.DateField(auto_now=True)
     picture = models.ImageField(upload_to='images/', null=True, blank=True)
     text = models.TextField("本文")
     is_public = models.BooleanField(default=True)
-    #tag
+    date_birth = models.DateField(auto_now_add=True)
+    date_update = models.DateField(auto_now=True)
 
-
-""" タグ """
-class Tag(models.Model):
-    pass
-
+    def __str__(self):
+        return self.title
 
 
 """ ポートフォリオページ用 """
 class Work(models.Model):
-    pass
+    title = models.CharField("webアプリ名",max_length=100)
+    text = models.TextField("説明")
+    picture = models.ImageField(upload_to='images/', null=True, blank=True)
+    page = models.URLField("URL")
+    is_public = models.BooleanField(default=True)
+    date_birth = models.DateField(auto_now_add=True)
+    date_update = models.DateField(auto_now=True)
+    subject = models.ForeignKey(Subject)
+
+    def __str__(self):
+        return self.title
 
 
 """ ジャンル """
 class Subject(models.Model):
-    pass
+    name = models.CharField("ジャンル名",max_length=100)
