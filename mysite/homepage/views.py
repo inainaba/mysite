5258b983d4ad1763dc0bce84618065c5fffa0341
inaba1203mpka <inainainainainaban@gmail.com>
2019-03-11 18:49:41 +0900

    ページネイター編集

--- mysite/homepage/views.py
+++ mysite/homepage/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views import generic
 from .models import *
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 class Top(generic.TemplateView):
@@ -17,8 +18,8 @@ class Myblog(generic.ListView):
     template_name = "homepage/myblog.html"
     model = Blog
     context_object_name = "blogs"
+    paginate_by = 3
     
-
     def get_queryset(self):
         return super().get_queryset().filter(is_public=True).order_by('-date_birth')
 
@@ -27,6 +28,7 @@ class Works(generic.ListView):
     template_name = "homepage/works.html"
     model = Work
     context_object_name = "works"
+    paginate_by = 5
 
     def get_queryset(self):
         return super().get_queryset().filter(is_public=True).order_by('-date_birth')
